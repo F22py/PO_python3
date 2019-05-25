0c5eb10aeab1dc4a5d3317ac29da9823a5c857e1
Aleksei <alexkeysel@gmail.com>
2019-05-26 00:20:45 +0200

    Added some gui stuff
    TODO:
    LIS
    CYBER OWCA
    GUI
    
    and debug...

--- Organisms/Animals/Czlowiek.py
+++ Organisms/Animals/Czlowiek.py
@@ -1,3 +1,68 @@
-class Czloweik:
-    def __init__(self):
+from Organisms.Animals.Zwierze import *
+
+
+class Czlowiek(Zwierze):
+    def __init__(self, my_world, x, y):
+        Zwierze.__init__(self, my_world, x, y)
+
+        self.sila = 5
+        self.inicjatywa = 4
+
+        self.symbol = "C"
+        self.title = "Human being"
+
+        self.img = "NO"
+
+        self.direction = 0
+        self.special = 0
+
+    def akcja(self, x=-1, y=-1):
+        if self.direction != 0:
+            self.check_special()
+
+            coordinates = self.get_move_coord(self.direction)
+            self.direction = 0
+
+            x = coordinates[0]
+            y = coordinates[1]
+
+            super(Czlowiek, self).akcja(x, y)
+
+    def activate_umejetnosc(self):
+        if self.special == 0:
+            self.special = 6
+            return True
+        return False
+
+    def check_special(self):
+        if self.special > 0:
+            if self.special > 3:
+                self.step = 2
+            else:
+                rand = random.randint(1, 100)
+
+                if rand >= 50:
+                    self.step = 2
+                else:
+                    self.step = 1
+
+            self.special -= 1
+
+            if self.special == 1:
+                self.end_special()
+
+        if self.special < 0:
+            self.special -= 1
+
+        if self.special == -6:
+            self.special = 0
+
+    def end_special(self):
+        self.step = 1
+        self.special = -1
+
+    def generate_data_to_save(self):
         pass
+
+
+
--- Swiat.py
+++ Swiat.py
@@ -1,12 +1,12 @@
 import queue
 
-from Organisms.Animals.Zwierze import *
+# from Organisms.Animals.Zwierze import *
+# from Organisms.Animals.Wilk import *
+# from Organisms.Animals.Owca import *
+# from Organisms.Animals.Zolw import *
+# from Organisms.Animals.Lis import *
+# from Organisms.Animals.Antylopa import *
 from Organisms.Animals.Czlowiek import *
-from Organisms.Animals.Wilk import *
-from Organisms.Animals.Owca import *
-from Organisms.Animals.Zolw import *
-from Organisms.Animals.Lis import *
-from Organisms.Animals.Antylopa import *
 
 from Organisms.OrganizmyList import *
 
@@ -17,18 +17,24 @@ class Swiat:
         self.height = height
 
         self.moje_organizmy = [[None for i in range(width)] for j in range(height)]
-        self.kolejkaRuchu = queue.PriorityQueue()
+        self.kolejka_ruchu = queue.PriorityQueue()
 
         self.tour_number = 0
         self.game_over = False
 
-        self.moje_organizmy[4][4] = Wilk(self, 4, 4)
-        self.moje_organizmy[2][2] = Owca(self, 2, 2)
+        self.Sasha = Czlowiek(self, int(width/2), int(height/2))
+        self.Sasha.title = "Sasha"
+        self.moje_organizmy[int(height/2)][int(width/2)] = self.Sasha
 
-        self.moje_organizmy[0][0] = Zolw(self, 0, 0)
+        self.dodaj_na_poczatek()
 
-        self.moje_organizmy[0][1] = Antylopa(self, 1, 0)
-        self.moje_organizmy[3][1] = Lis(self, 1, 3)
+        # self.moje_organizmy[4][4] = Wilk(self, 4, 4)
+        # self.moje_organizmy[2][2] = Owca(self, 2, 2)
+        #
+        # self.moje_organizmy[0][0] = Zolw(self, 0, 0)
+        #
+        # self.moje_organizmy[0][1] = Antylopa(self, 1, 0)
+        # self.moje_organizmy[3][1] = Lis(self, 1, 3)
     # def get_organizmy(self):
     #     return self.moje_organizmy
     #
@@ -41,14 +47,15 @@ class Swiat:
     # def get_game_over(self):
     #     return self.game_over
 
-    def set_czlowiek_direction_global(self, dir):
-        pass
+    def set_czlowiek_direction_global(self, direction):
+        if self.Sasha is not None:
+            self.Sasha.direction = direction
 
     def wykonaj_ture(self):
         if not self.game_over:
-            while not self.kolejkaRuchu.empty():
-                organizm = self.kolejkaRuchu.get()
-                print(organizm.polozenie)
+            while not self.kolejka_ruchu.empty():
+                organizm = self.kolejka_ruchu.get()
+                # print(organizm.polozenie)
                 if self.moje_organizmy[organizm.polozenie[1]][organizm.polozenie[0]] is not None:
                     organizm.tour_life += 1
                     organizm.akcja()
@@ -57,38 +64,60 @@ class Swiat:
             self.update_queue()
             self.rysuj_swiat()
 
+        return not self.game_over
+
     def init_organizmy(self, x, y, num):
-        pass
+        val = OrganizmyList(num)
+        self.moje_organizmy[y][x] = \
+            OrganizmyList.create_new_organizm(val, self, x, y)
 
     def zabij_organizm(self, x, y):
-        if isinstance(self.moje_organizmy[y][x], Czloweik):
-            pass
+        if isinstance(self.moje_organizmy[y][x], Czlowiek):
+            self.Sasha = None
+            self.game_over = True
 
         self.moje_organizmy[y][x] = None
 
         storage = []
-        while not self.kolejkaRuchu.empty():
-            org = self.kolejkaRuchu.get()
+        while not self.kolejka_ruchu.empty():
+            org = self.kolejka_ruchu.get()
             if self.moje_organizmy[org.polozenie[1]][org.polozenie[0]] is not None:
                 storage.append(org)
 
         for organism in storage:
-            self.kolejkaRuchu.put(organism)
+            self.kolejka_ruchu.put(organism)
 
     def try_to_activate_special(self):
-        pass
+        result = False
+
+        if self.Sasha is not None:
+            result = self.Sasha.activate_umejetnosc()
+
+        return result
 
     def save_game(self):
         pass
 
     def dodaj_na_poczatek(self):
-        pass
+        r_x = 0
+        r_y = 0
+
+        for k in range(0, 10 + int(self.width*self.height/10)):
+            running = True
+            while running:
+                r_x = random.randint(0, self.width-1)
+                r_y = random.randint(0, self.height-1)
+
+                if self.moje_organizmy[r_y][r_x] is None:
+                    running = False
+
+            self.init_organizmy(r_x, r_y, k % 10 + 1)
 
     def update_queue(self):
         for j in range(0, self.height):
             for k in range(0, self.width):
                 if self.moje_organizmy[j][k] is not None:
-                    self.kolejkaRuchu.put(self.moje_organizmy[j][k])
+                    self.kolejka_ruchu.put(self.moje_organizmy[j][k])
 
     def rysuj_swiat(self):
         for j in range(0, self.height):
@@ -103,22 +132,19 @@ class Swiat:
 
 
 
-testing = Swiat(5, 5)
+# testing = Swiat(5, 5)
+#
+# # testing.wykonaj_ture()
+# # print("--------------")
+# # testing.wykonaj_ture()
+# # print("--------------")
+# # testing.wykonaj_ture()
+# # print("--------------")
+# # testing.wykonaj_ture()
+# for i in range(0, 40):
+#     testing.wykonaj_ture()
+#     print("-----------------------------------")
 
-# testing.wykonaj_ture()
-# print("--------------")
-# testing.wykonaj_ture()
-# print("--------------")
-# testing.wykonaj_ture()
-# print("--------------")
-# testing.wykonaj_ture()
-
-for i in range(0, 30):
-    testing.wykonaj_ture()
-
-kkk = OrganizmyList
-print(kkk.size())
-print(kkk.create_new_organizm(OrganizmyList.Barszcz, testing, 5, 5))
 
 # ttt = Zwierze(testing, 5, 3)
 # ttt.rozmnoz_sie()
