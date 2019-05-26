532dadabccae81401ef1d797ffac0c98243949bc
Aleksei <alexkeysel@gmail.com>
2019-05-26 23:10:37 +0200

    Added some gui stuff
    TODO:
    LIS
    CYBER OWCA
    GUI
    
    Bug with MAN colision - (mb due to direction?)
    and debug...

--- Organisms/Animals/Zwierze.py
+++ Organisms/Animals/Zwierze.py
@@ -19,6 +19,8 @@ class Zwierze(Organizm):
             self.set_new_polozenie(x, y)
 
             self.organizmy[y][x] = self.organizmy[tY][tX]
+
+
             # self.World.zabij_organizm(tX, tY)
             self.organizmy[tY][tX] = None
         else:
